6f40289caec7c776f1d185f4358ccb98035b6ea5
ifilmsstudios <97464700+ifilmsstudios@users.noreply.github.com>
2022-07-17 12:31:01 +0530

    Add files via upload

---
+++ tobrot/plugins/new_join_fn.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+import logging
+
+import pyrogram
+from tobrot import *
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+async def new_join_f(client, message):
+    chat_type = message.chat.type
+    if chat_type != "private":
+        await message.reply_text(
+            f"""<b>🙋🏻‍♂️ Hello dear!\n\n This Is A Leech Bot.\n\nThis Chat Is Not Supposed To Use Me</b>\n\n<b>Current CHAT ID:</b> <code>{message.chat.id}</code>""",
+            parse_mode="html",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton('🎻 Sᴜᴘᴘᴏʀᴛ', url='https://t.me/iFilms_Support')
+                    ]
+                ]
+               )
+            )
+        # leave chat
+        await client.leave_chat(chat_id=message.chat.id, delete=True)
+    # delete all other messages, except for AUTH_CHANNEL
+    await message.delete(revoke=True)
+
+
+async def help_message_f(client, message):
+    if UPLOAD_AS_DOC:
+        utxt = "Document"
+    else:
+        utxt = "Streamable"
